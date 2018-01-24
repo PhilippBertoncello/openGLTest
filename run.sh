c89f5672754da456c70033eb4587540af0fb2364
Philipp Bertoncello <philippbertoncello2@gmail.com>
2018-01-24 18:58:13 +0100

    Cleaned up the code, merging to master

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ -o shooter.out main.cpp -lGL -lGLU -lglut -Lsource/vecmath.cpp -Lsource/engine.cpp -DGL_GLEXT_PROTOTYPES
-./shooter.out
+g++ -o build/shooter.out source/main.cpp -lGL -lGLU -lglut -Lsource/vecmath.cpp -Lsource/engine.cpp -DGL_GLEXT_PROTOTYPES
+./build/shooter.out
